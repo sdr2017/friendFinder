f9a5a27b1d9a0bdbd9a48fbfbe46fa8d7dc94f03
Sarah Diaz-Rivera <sarahjeanlinnebur@yahoo.com>
2017-08-20 14:32:29 -0600

    getting links to work correctly on server side

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,14 +2,13 @@ var path = require("path");
 debugger;
 
 var htmlRoutes = function(app) {
-	app.use("/", function(req, res) { //display the home html page
+	app.get("/survey", function(req, res) { //display the survey page
+	  res.sendFile(path.join(__dirname, "/../public/survey.html"));
+	});
+
+	app.use(function(req, res) { //display the home html page
 	  res.sendFile(path.join(__dirname, "/../public/home.html"));
 	});
-
-	app.get("/survey", function(req, res) { //display the survey page
-	  res.sendFile(path.join(__dirname, "survey.html"));
-	});
-
 };
 
 module.exports = htmlRoutes;
